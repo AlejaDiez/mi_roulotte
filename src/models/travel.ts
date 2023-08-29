041641577c31dfca8c2a5a983a2eadd94e053f84
Alejandro Diez Bermejo <alejandrodiezbermejo@gmail.com>
2023-08-30 00:16:50 +0200

    ✈️ Travels

---
+++ src/models/travel.ts
@@ -0,0 +1,12 @@
+import type { Image } from "@models/image";
+import type { Video } from "@models/video";
+
+export default interface Travel {
+    date?: Date;
+    title: string;
+    subtitle: string;
+    description: string;
+    image: Image;
+    video?: Video;
+    keywords?: string[];
+}

5d34e1024617d7883f2356b0c8fd72a0a5e4df90
Alejandro Diez Bermejo <alejandrodiezbermejo@gmail.com>
2025-06-02 14:06:22 +0200

    Add slider styles to components.css and script to Layout.astro

---
+++ src/utils/slider.ts
@@ -0,0 +1,31 @@
+const sliders = document.querySelectorAll<HTMLInputElement>(
+    'input[type="range"].slider'
+);
+
+for (const slider of sliders) {
+    const emitValueEvent = () => {
+        slider.dispatchEvent(
+            new CustomEvent("value", {
+                detail: slider.value,
+                bubbles: false,
+                cancelable: false
+            })
+        );
+    };
+
+    slider.addEventListener("input", emitValueEvent);
+    slider.addEventListener("value", (event: Event) => {
+        const { detail } = event as CustomEvent;
+
+        const value = Number(detail) || 0;
+        const min = Number(slider.min) || 0;
+        const max = Number(slider.max) || 100;
+
+        slider.value = String(value);
+        slider.style.setProperty(
+            "--progress",
+            `${((value - min) / (max - min)) * 100}%`
+        );
+    });
+    emitValueEvent();
+}

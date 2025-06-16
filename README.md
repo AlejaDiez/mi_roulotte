1801e24f1c1eac789eb0d922949e1d2914a1656e
Alejandro Diez Bermejo <alejandrodiezbermejo@gmail.com>
2025-06-16 18:33:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+<div align="center">
+    <img src="public/favicon.svg" width="120" height="120" />
+    <br />
+    <h1 align="center">Mi Roulotte</h1>
+    <p align="center">
+        Mi Roulotte es una página web que creé en 2017, cuando tenía tan solo 14 años, después de embarcarnos en nuestro primer viaje largo a través de Francia. Fue durante esta aventura que nació la idea de compartir nuestras experiencias y descubrimientos a través de un espacio en línea. Desde entonces, Mi Roulotte se ha convertido en un lugar donde capturo y comparto mis recuerdos de viajes, inspirando a otros a explorar y disfrutar del mundo que nos rodea. Te invito a unirte a este viaje virtual y sumergirte en las maravillosas historias y momentos que he experimentado a lo largo de los años.
+    </p>
+    <h3>¡Nos vemos en la carretera!</h3>
+    <a href="https://miroulotte.es" target="_blank">miroulotte.es</a>
+</div>
+<hr>
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><a href="miroulotte.es" target="_blank">Mi Roulotte</a> por Alejandro Diez Bermejo se distribuye bajo una <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Licencia Creative Commons Atribución-NoComercial-CompartirIgual 4.0 Internacional</a>.
+
+© 2025 | Alejandro Diez Bermejo
---
+++ astro.config.mjs
@@ -0,0 +1,54 @@
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+    site: "https://miroulotte.es",
+    trailingSlash: "always",
+    output: "static",
+    outDir: "./build",
+    scopedStyleStrategy: "where",
+    vite: {
+        build: {
+            assetsDir: "../assets",
+            compressHTML: true,
+            cssMinify: true,
+            minify: true,
+            rollupOptions: {
+                output: {
+                    assetFileNames: ({ names }) => {
+                        const { ext } = path.parse(names[0]);
+
+                        // Styles
+                        if (/css/i.test(ext)) {
+                            return "styles/style.[hash].css";
+                        }
+                        // Images
+                        if (/png|jpe?g|webp|svg|gif|tiff|bmp|ico/i.test(ext)) {
+                            return "images/[name][extname]";
+                        }
+                        // Default
+                        return "/[name].[hash][extname]";
+                    },
+                    chunkFileNames: "scripts/script.[hash].js",
+                    entryFileNames: "scripts/script.[hash].js",
+                },
+            },
+        },
+        plugins: [tailwindcss()],
+    },
+    build: {
+        assets: "images",
+        inlineStylesheets: "never",
+    },
+    server: {
+        host: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+    },
+    devToolbar: { enabled: false },
+    i18n: {
+        locales: ["es"],
+        defaultLocale: "es",
+    },
+});

1d9c8e57a38276f80f5ca16ce95b1a2ad2d6b020
Alejandro Diez Bermejo <alejandrodiezbermejo@gmail.com>
2025-07-04 13:36:41 +0200

    Add D1 database configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "drizzle-kit";
+
+export default {
+    dialect: "sqlite",
+    out: "src/server/db/migrations",
+    schema: "src/server/db/schemas",
+    ...(process.env.LOCAL_DB_PATH
+        ? {
+              dbCredentials: { url: process.env.LOCAL_DB_PATH }
+          }
+        : {
+              driver: "d1-http",
+              dbCredentials: {
+                  databaseId: process.env.DB_ID!,
+                  token: process.env.D1_TOKEN!,
+                  accountId: process.env.CLOUDFLARE_ACCOUNT_ID!
+              }
+          })
+} satisfies Config;

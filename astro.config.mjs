61cad388534b19e0fd675e580561270d3658b85a
Alejandro Diez Bermejo <alejandrodiezbermejo@gmail.com>
2025-07-10 11:39:26 +0200

    Add Cloudflare D1 database connection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
+import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import path from "node:path";
 
 export default defineConfig({
     site: "https://miroulotte.es",
     trailingSlash: "ignore",
-    output: "static",
-    outDir: "./build",
+    output: "server",
+    adapter: cloudflare({
+        platformProxy: { enabled: true },
+    }),
+    outDir: "build",
     scopedStyleStrategy: "where",
     vite: {
         build: {
@@ -16,18 +21,19 @@ export default defineConfig({
             rollupOptions: {
                 output: {
                     assetFileNames: ({ names }) => {
-                        const { ext } = path.parse(names[0]);
+                        const ext = names[0] ? path.extname(names[0]) : "";
 
                         // Styles
-                        if (/css/i.test(ext)) {
-                            return "styles/style.[hash].css";
-                        }
+                        if (/css/i.test(ext)) return "styles/style.[hash].css";
                         // Images
-                        if (/png|jpe?g|webp|svg|gif|tiff|bmp|ico/i.test(ext)) {
+                        if (
+                            /\.(png|jpe?g|webp|svg|gif|tiff|bmp|ico)$/i.test(
+                                ext,
+                            )
+                        )
                             return "images/[name][extname]";
-                        }
                         // Default
-                        return "/[name].[hash][extname]";
+                        return "[name].[hash][extname]";
                     },
                     chunkFileNames: "scripts/script.[hash].js",
                     entryFileNames: "scripts/script.[hash].js",
